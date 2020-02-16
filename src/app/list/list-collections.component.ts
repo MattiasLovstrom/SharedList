8ae98cf53b73045a2a2101248644626de3630fde
MattiasLovstrom <123Kaffe>
2020-02-16 15:54:34 +0100

    Minor design changes

--- src/app/list/list-collections.component.ts
+++ src/app/list/list-collections.component.ts
@@ -11,12 +11,12 @@ import { HttpClient } from '@angular/common/http';
             <input type="text" name="name" placeholder="Name of new list collection" ngModel />
             <button  title="Create new list collection" class="fa fa-plus"></button>
         </form>
-        <ul>
-    <li *ngFor="let collection of collections">
-        <a routerLink="/{{collection.id}}" routerLinkActive="active">{{collection.name}}</a>&nbsp;&nbsp;&nbsp;
-        <button title="Remove" class="fa fa-times"  (click)="delete(collection.id)"></button>
-    </li>
-</ul>
+        <ol>
+            <li *ngFor="let collection of collections">
+               <a routerLink="/{{collection.id}}" routerLinkActive="active">{{collection.name}}</a>&nbsp;&nbsp;&nbsp;
+               <button title="Remove" class="fa fa-times"  (click)="delete(collection.id)"></button>
+            </li>
+         </ol>
     `
 })
 export class ListCollectionsComponent implements OnInit {
